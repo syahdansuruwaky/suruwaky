b8f76de6c20c7a6b75af217ce98ff1a9fc2404d5
syahdansuruwaky <syahdansuruwaky2008@gmail.com>
2026-02-22 12:38:18 +0900

    Delete peminjaman_hapus.php

--- peminjaman_hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = $_GET['id'];
-$query = mysqli_query($koneksi, "DELETE FROM peminjaman WHERE id_peminjaman=$id");
-?>
-<script>
-    alert('Hapus data berhasil');
-    location.href = "index.php?page=peminjaman";
-</script>
