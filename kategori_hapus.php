ec5cdc2a10e75315894bd4867cc20652d0742533
syahdansuruwaky <syahdansuruwaky2008@gmail.com>
2026-02-22 12:36:22 +0900

    Delete kategori_hapus.php

--- kategori_hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = $_GET['id'];
-$query = mysqli_query($koneksi, "DELETE FROM kategori WHERE id_kategori=$id");
-?>
-<script>
-    alert('Hapus Data Berhasil');
-    location.href = "index.php?page=kategori";
-</script>
